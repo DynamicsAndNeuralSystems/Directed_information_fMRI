53747e13464a68823c31f050ed6656b8d687380f
anniegbryant <anniegbryant@gmail.com>
2025-03-23 11:27:07 +1100

    updated figs

--- feature_extraction/group_avg_structural_connectome.m
+++ feature_extraction/group_avg_structural_connectome.m
@@ -3,6 +3,18 @@
 data_path='/Users/abry4213/data/HCP100';
 load(sprintf('%s/raw_data/diffusion_MRI/aparc_acpc_connectome_data.mat', data_path));
 
+%% Taking the mean # connections per edge
+
+% Convert the SIFT2 cell array to a 3D matrix (68x68x100)
+mat3D = cat(3, SIFT2{:});
+
+% Compute the element-wise mean across the 100 matrices
+avg_SIFT2 = mean(mat3D, 3);
+
+% Save resulting group adjmat
+writematrix(avg_SIFT2, sprintf('%s/raw_data/diffusion_MRI/aparc_HCP100_group_avg_SIFT2_mean.csv', data_path));
+
+%%
 % Consistency-based thresholding, as in https://github.com/DynamicsAndNeuralSystems/humanStructureFunction/blob/master/Analysis/GroupNodeStrength.m
 
 % Consistency-based group connectome parameters
